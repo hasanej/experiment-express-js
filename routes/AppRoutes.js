31afa067d7ed81c093268cb501f36ac54a458f37
Hasan El Jabir <eljabirhasan@gmail.com>
2019-06-24 14:06:45 +0700

    Update code in AppRoutes.js

--- routes/AppRoutes.js
+++ routes/AppRoutes.js
@@ -1,29 +1,33 @@
 const router = require('express').Router();
-const Book = require('../models/Book')
+const Book = require('../models/Book');
 
-router.get("/", (req,res) => {
-	const {page, perpage,sort} = req.query;
+//Get all book
+router.get("/", (req, res) => {
+	const {page, perpage, sort} = req.query;
 
 	Book.paginate({}, {
 		page: parseInt(page, 10),
-		limit: parseInt(perpage, 10) ,
-		sort:{title: parseInt(sort)}})
+		limit: parseInt(perpage, 10),
+		sort: {title: parseInt(sort)}
+	})
 	.then(result => res.send(result.docs))
 })
 
-router.post("/", (req,res) => {
+//Add book
+router.post("/", (req, res) => {
 	Book.create(req.body)
 	.then(data => res.send(data))
 })
 
-
-router.get("/edit/:id", (req,res) => {
-	Book.findById({_id : req.params.id})
+//Edit book
+router.get("/edit/:id", (req, res) => {
+	Book.findById({_id: req.params.id})
 	.then(data => res.send(data))
 })
 
-router.put("/edit/:id", (req,res) => {
-	Book.findByIdAndUpdate({_id : req.params.id}, {
+//Update book
+router.put("/edit/:id", (req, res) => {
+	Book.findByIdAndUpdate({_id: req.params.id}, {
 		$set: {
 			title: req.body.title,
 			author: req.body.author,
@@ -36,7 +40,7 @@ router.put("/edit/:id", (req,res) => {
 	})
 })
 
-router.delete("/:id", (req,res) => {
+router.delete("/:id", (req, res) => {
 	const id = req.params.id
 	Book.findByIdAndRemove(id)
 	.then(data => res.send("Book Deleted"))
