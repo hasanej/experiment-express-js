2370f906e6b41788449788b05eb6b5bb67e8cae1
Hasan El Jabir <eljabirhasan@gmail.com>
2019-06-24 14:09:19 +0700

    Update code in Book.js

--- models/Book.js
+++ models/Book.js
@@ -3,22 +3,20 @@ const Schema = mongoose.Schema;
 const mongoosePaginate = require('mongoose-paginate');
 
 const bookSchema = new Schema({
-	title : {
-		type : String,
-		required : true
+	title: {
+		type: String,
+		required: true
 	},
-	author : {
-		type : String,
-		required : true
+	author: {
+		type: String,
+		required: true
 	},
-	description : {
-		type : String,
-		required : true
+	description: {
+		type: String,
+		required: true
 	}
-})
+});
 
-bookSchema.plugin(mongoosePaginate)
-
+bookSchema.plugin(mongoosePaginate);
 const Book = mongoose.model("book", bookSchema);
-
 module.exports = Book;
